573da9b9eacc371c27747cc59d86f4d99b3c4ab1
mnglei <edward.munguulei@gmail.com>
2025-01-03 16:30:22 +0800

    Reapply "feat: pre-order page"
    This reverts commit 593405c0e65af090abd9ab5ccbccc542d7c3f7d3.

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -1,9 +1,15 @@
+"use client";
+
 import Link from "next/link";
 import MobileMenu from "./mobile-menu";
 import Image from "next/image";
 import HeroImage from "@/public/images/erdeme-logo.png";
+import { usePathname } from "next/navigation";
 
 export default function Header() {
+  const pathname = usePathname();
+  const isHomePage = pathname === "/";
+
   return (
     <header className="absolute w-full z-30">
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
@@ -21,7 +27,18 @@ export default function Header() {
               />
             </Link>
           </div>
-          {/* <MobileMenu /> */}
+
+          {/* Pre-order button - only shown on homepage */}
+          <div className="flex-1 flex justify-end">
+            {isHomePage && (
+              <Link
+                href="/pre-order"
+                className="btn-sm text-white bg-purple-600 hover:bg-purple-700 rounded-lg ml-3"
+              >
+                Багшаар бүртгүүлэх
+              </Link>
+            )}
+          </div>
         </div>
       </div>
     </header>
