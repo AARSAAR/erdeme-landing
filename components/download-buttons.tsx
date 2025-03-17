d70016f4600045844b5eaacf9df1d1243071592e
mnglei <edward.munguulei@gmail.com>
2025-03-17 16:45:31 +0800

    download buttons enabled

--- components/download-buttons.tsx
+++ components/download-buttons.tsx
@@ -4,25 +4,36 @@ import styles from "./AppDownloadButtons.module.css";
 const AppDownloadButtons = () => {
   return (
     <div className="flex flex-col items-center justify-center gap-4 my-8">
-      <div className="text-2xl font-semibold text-gray-300">Тун удахгүй...</div>
       <div className="flex gap-6">
-        <div className="flex flex-col items-center opacity-60 cursor-not-allowed">
-          <img
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Download_on_the_App_Store_Badge.svg/800px-Download_on_the_App_Store_Badge.svg.png"
-            alt="App Store - Coming Soon"
-            className="w-[200px] grayscale"
-          />
-          {/* <span className="text-sm text-gray-400 mt-2">Тун удахгүй</span> */}
-        </div>
+        <a
+          href="https://apps.apple.com/mn/app/erdeme-bagsh/id6470268855"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="transition-opacity hover:opacity-80 active:opacity-70"
+        >
+          <div className="flex flex-col items-center">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Download_on_the_App_Store_Badge.svg/800px-Download_on_the_App_Store_Badge.svg.png"
+              alt="App Store"
+              className="w-[200px]"
+            />
+          </div>
+        </a>
 
-        <div className="flex flex-col items-center opacity-60 cursor-not-allowed">
-          <img
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Google_Play_Store_badge_EN.svg/2560px-Google_Play_Store_badge_EN.svg.png"
-            alt="Google Play - Coming Soon"
-            className="w-[200px] grayscale"
-          />
-          {/* <span className="text-sm text-gray-400 mt-2">Тун удахгүй</span> */}
-        </div>
+        <a
+          href="https://play.google.com/store/apps/details?id=com.htp.erdeme_tutor"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="transition-opacity hover:opacity-80 active:opacity-70"
+        >
+          <div className="flex flex-col items-center">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Google_Play_Store_badge_EN.svg/2560px-Google_Play_Store_badge_EN.svg.png"
+              alt="Google Play"
+              className="w-[200px]"
+            />
+          </div>
+        </a>
       </div>
     </div>
   );
